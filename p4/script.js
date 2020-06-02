ef28fe776211900ff307a992c6eae246ec8c4e6d
BuildTools <justinkrasner98@gmail.com>
2020-06-01 22:17:58 -0400

    added part 4

---
+++ p4/script.js
@@ -0,0 +1,21 @@
+const passwordSubmit = document.getElementById("submit");
+const passwordValue = document.forms["myform"]["password"].value;
+
+console.log(passwordSubmit)
+passwordSubmit.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('clicked submit');
+  const passwordValue = document.forms["myform"]["password"].value;
+  if(checkPassword(passwordValue)===true){
+     alert('OK!')
+     myform.innerHTML = '<h1>Thank you for entering your info.</h1>';
+   }
+  else alert('Incorrect password!')
+});
+
+
+function checkPassword(value){
+  if(value=='12345678')
+    return true;
+  return false;
+}
